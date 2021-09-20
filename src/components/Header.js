171d68ee85637f23c7e15b0cc293755e7ad82cd1
abecirovic3 <abecirovic3@email.com>
2021-09-20 13:13:13 +0200

    added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="navbar">
+                <ul>
+                    <li className="active"><a href="#">Home</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
