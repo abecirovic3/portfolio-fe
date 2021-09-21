7952d3d27b50b68d8dd53847a6e5832b8ccb229f
abecirovic3 <abecirovic3@email.com>
2021-09-21 11:21:34 +0200

    added error checking when fetching technologies

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -3,11 +3,19 @@ import { useState, useEffect} from "react";
 
 const Technologies = () => {
     const [technologies, setTechnologies] = useState([]);
+    const [networkError, setNetworkError] = useState(false);
 
     useEffect(() => {
         fetch('http://localhost:5000/technology/all')
-            .then(response => response.json())
-            .then(data => setTechnologies(JSON.parse(data)));
+            .then(response => {
+                if (!response.ok)
+                    throw new Error("An error occurred while fetching the data");
+                return response.json();
+            })
+            .then(data => setTechnologies(JSON.parse(data)))
+            .catch(err => {
+                setNetworkError(true);
+            });
 
     }, []);
 
@@ -16,8 +24,8 @@ const Technologies = () => {
             <h1>Technologies I worked with</h1>
             <h3>With listing all of these technologies I just want to say that I have a basic understanding of all of them,
                 and that I'm open to working with any and getting more skilled at them when needed.</h3>
-
-            {technologies.map((technology) => (
+            { networkError ? <h3>Network error occurred, couldn't load technologies :(</h3> :
+            technologies.map((technology) => (
                 <Technology key={technology.id} data={technology}/>
             ))}
         </>
