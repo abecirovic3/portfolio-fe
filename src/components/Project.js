fe874fd338a3ed71beeb659c2ee70fe66e5d44e6
abecirovic3 <abecirovic3@email.com>
2021-09-21 13:57:04 +0200

    implemented projects page

---
+++ src/components/Project.js
@@ -0,0 +1,30 @@
+const Project = ( { data } ) => {
+
+    let img = {
+        default: "img"
+    };
+
+    try {
+        img = require(`../img/project-logos/${data.imgUrl}`);
+    } catch (e) {
+        console.log(e);
+    }
+
+    return (
+        <div className="project-descr">
+            <div className="project-descr-inner">
+                <h2>{data.name}</h2>
+                <hr />
+                <img src={img.default} alt={data.imgUrl} />
+                <h3>{data.description}</h3>
+                <hr />
+                <h3>Technologies used</h3>
+                <h4>{data.technologies}</h4>
+                <hr />
+                <a href={data.githubLink} target="_blank">Check it out on GitHub</a>
+            </div>
+        </div>
+    );
+}
+
+export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,36 @@
+import NetworkError from "./NetworkError";
+import Project from "./Project";
+
+import { useState, useEffect } from "react";
+
 const Projects = () => {
+    const [projects, setProjects] = useState([]);
+    const [networkError, setNetworkError] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/project/all')
+            .then(response => {
+                if (!response.ok)
+                    throw new Error("An error occurred while fetching the data");
+                return response.json();
+            })
+            .then(data => setProjects(JSON.parse(data)))
+            .catch(err => {
+                setNetworkError(true);
+            });
+
+    }, []);
+
     return (
-        <div>
-            <h1>Projects</h1>
-        </div>
+        <>
+            <h1>Projects I worked on</h1>
+            <h3>Below are some of the projects I worked on. Some are individual and some are
+                team projects. Feel free to check them out over at my GitHub.</h3>
+            { networkError ? <NetworkError contactInfo="ajdinrs.becirovic@gmail.com" /> :
+                projects.map((project) => (
+                    <Project key={project.id} data={project}/>
+                ))}
+        </>
     );
 }
 
