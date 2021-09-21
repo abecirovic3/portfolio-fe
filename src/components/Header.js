bc3810d9e4391fbe1a1755cb3748651474cd26c2
abecirovic3 <abecirovic3@email.com>
2021-09-21 14:59:01 +0200

    fixed error for empty className in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ const Header = () => {
         <div className="header">
             <div className="navbar">
                 <ul>
-                    <li className={location.pathname === "/" && "active"}><Link to="/">Home</Link></li>
-                    <li className={location.pathname === "/projects" && "active"}><Link to="/projects">Projects</Link></li>
-                    <li className={location.pathname === "/blog" && "active"}><Link to="/blog">Blog</Link></li>
-                    <li className={location.pathname === "/contact" && "active"}><Link to="/contact">Contact</Link></li>
+                    <li className={location.pathname === "/" ? "active" : ""}><Link to="/">Home</Link></li>
+                    <li className={location.pathname === "/projects" ? "active" : ""}><Link to="/projects">Projects</Link></li>
+                    <li className={location.pathname === "/blog" ? "active" : ""}><Link to="/blog">Blog</Link></li>
+                    <li className={location.pathname === "/contact" ? "active" : ""}><Link to="/contact">Contact</Link></li>
                 </ul>
             </div>
         </div>
