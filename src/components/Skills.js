a524a57971a2a607d19f0e033d743980151b4586
abecirovic3 <abecirovic3@email.com>
2021-09-23 15:22:10 +0200

    implemented fetching of skills data

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import APIRoute from "./APIRoute";
+
 const Skills = () => {
+    const [skills, setSkills] = useState([]);
+    const [networkError, setNetworkError] = useState(false);
+
+    useEffect(() => {
+        fetch(`${APIRoute}/skill/all`)
+            .then(response => {
+                if (!response.ok)
+                    throw new Error("An error occurred while fetching the data");
+                return response.json();
+            })
+            .then(data => setSkills(JSON.parse(data)))
+            .catch(err => {
+                setNetworkError(true);
+            });
+
+    }, []);
     return (
         <>
             <h1>Skills</h1>
+            {networkError ?
             <div>
                 <h3>Most of my time at collage I worked with C++ and Java, but I'm comfortable with many other technologies</h3>
                 <h3>I am familiar with Git and Github</h3>
@@ -10,7 +30,11 @@ const Skills = () => {
                 <h3>I am a team player, open minded and always open for a good laugh</h3>
                 <h3>Bellow are listed all the technologies I worked with and the skill level I'd say I'm having based on the
                     number of projects and the time I've spent working with each of them</h3>
-            </div>
+            </div> :
+                <div>
+                    { skills.map((skill) => ( <h3 key={skill.id}>{skill.description}</h3> ))}
+                </div>
+            }
 
             <hr />
         </>
