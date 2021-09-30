7e53a6e750cc111f389edc3391985301b8800bc5
abecirovic3 <abecirovic3@email.com>
2021-09-30 16:43:19 +0200

    spell checked static text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,8 @@ const Projects = ({ projects }) => {
     return (
         <>
             <h1>Projects I worked on</h1>
-            <h3>Below are some of the projects I worked on. Some are individual and some are
-                team projects. Feel free to check them out over at my GitHub.</h3>
+            <h3>Below are some of the projects I worked on. Some are individual and some are team projects.
+                Feel free to check them out over at my GitHub.</h3>
             { projects.map((project) => (
                     <Project key={project.id} data={project}/>
                 ))}
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -4,8 +4,11 @@ const Technologies = ({ technologies }) => {
     return (
         <>
             <h1>Technologies I worked with</h1>
-            <h3>With listing all of these technologies I just want to say that I have a basic understanding of all of them,
-                and that I'm open to working with any and getting more skilled at them when needed.</h3>
+            <h3>Below are listed all the technologies I’ve worked with and the skill level I'd say I'm having based
+                on the number of projects and the time I've spent working with each of them.
+                By listing all of these technologies I just want to say that I have a basic understanding
+                of all of them, and that I'm open to working with any and getting more skilled at them
+                when needed.</h3>
             { technologies.map((technology) => (
                 <Technology key={technology.id} data={technology}/>
                 ))}
